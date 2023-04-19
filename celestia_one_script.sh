3b78c36fec80f80873cc43363b7c341a9d46db6c
Thinh Pham Ngoc <ngocthinhbk@gmail.com>
2023-04-19 18:38:16 +0700

    update

--- celestia_one_script.sh
+++ celestia_one_script.sh
@@ -16,9 +16,9 @@ sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential gi
 ver="1.20" 
 cd $HOME 
 wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" 
-rm -rf /usr/local/go 
-tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" 
-rm "go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go 
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" 
+sudo rm "go$ver.linux-amd64.tar.gz"
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
